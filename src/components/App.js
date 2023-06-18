24c5c1fe7a27faf355a60680f65556c6282ae798
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-06-18 13:14:59 +0530

    submitting code-Sun Jun 18 2023 13:14:20 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -10,53 +10,49 @@ const App = () => {
 
   return (
     <div>
-           <h2>Select Your gender:</h2>
-
-            <input 
-              type="radio"
-              name="gender"
-              value = "Male"
-              id="male"
-              onChange={(e) => setGender(e.target.value)}
+            <h2>Select your gender:</h2>
+            <input type="radio" name="gender" id="male" value="male" 
+              onClick={(e)=> setGender(e.target.value)}
             />
             <label forHTML="male">Male</label>
-            <input 
-              type="radio"
-              name="gender"
-              value = "Female"
-              id="female"
-              onChange={(e) => setGender(e.target.value)}
+
+            <input type="radio" name="gender" id="female" value="female" 
+              onClick={(e)=> setGender(e.target.value)}
             />
             <label forHTML="female">Female</label>
 
             {
-                gender == "Male" &&
+              gender==male &&(
                 <div>
-                        <h2>Select your shirt size:</h2>
-                        <select>
-                            <option value="select" disabled>Select size</option>
-                            <option value="small">Small</option>
-                            <option value="medium">Medium</option>
-                            <option value="large">Large</option>
-                        </select>
+                     <h2>Select your shirt size:</h2>
+                     <select name="male-size" id="male-size">
+                        <option value="Select size" disabled >Select size</option>
+                        <option value="Small">Small</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Large">Large</option>
 
+                     </select>
                 </div>
+              )
+
             }
 
-{
-                gender == "Female" &&
+            {
+              gender==female &&(
                 <div>
-                        <h2>Select your dress size:</h2>
-                        <select>
-                            <option value="select" disabled>Select size</option>
-                            <option value="two">2</option>
-                            <option value="four">4</option>
-                            <option value="six">6</option>
-                        </select>
+                     <h2>Select your dress size:</h2>
+                     <select name="femlae-size" id="female-size">
+                        <option value="Select size" disabled>Select size</option>
+                        <option value="two">2</option>
+                        <option value="four">4</option>
+                        <option value="six">6</option>
 
+                     </select>
                 </div>
-            }
+              )
 
+            }
+            
     </div>
   )
 }
